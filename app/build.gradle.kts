3cb1630846ff7166c81d6757e54b43b659fa5c0a
KaruSenpai <gealonjhoncarlo@gmail.com>
2025-04-11 09:41:08 +0800

    login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,8 @@ android {
 }
 
 dependencies {
+    implementation ("com.example:laneinit:1.0")
+    implementation ("com.example:library:1.0")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -40,3 +42,4 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
--- app/src/main/java/com/example/echomind/MainActivity.java
+++ app/src/main/java/com/example/echomind/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.echomind;
 
 import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import com.example.Library;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
