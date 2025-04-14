9a02a0a7f7358331545201823428572c82dff4df
KaruSenpai <gealonjhoncarlo@gmail.com>
2025-04-14 09:04:48 +0800

    login

---
+++ app/src/main/java/com/example/echomind/Creat_Account.java
@@ -0,0 +1,14 @@
+package com.example.echomind;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Creat_Account extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creat_account);
+    }
+}
--- app/src/main/java/com/example/echomind/MainActivity.java
+++ app/src/main/java/com/example/echomind/MainActivity.java
@@ -3,8 +3,8 @@ package com.example.echomind;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
-
-
+import android.widget.Button;
+import android.content.Intent;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,5 +23,16 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.login_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Creat_Account.class);
+            }
+        });
+
+
+
     }
 }
